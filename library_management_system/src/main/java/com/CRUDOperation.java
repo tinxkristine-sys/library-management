b6b3d4eebd1725f18e3d265a3085a5a9854030ce
Kristine G. Sañosa <tinxkristine@gmail.com>
2026-05-05 14:05:00 +0800

    working on crud

--- library_management_system/src/main/java/com/CRUDOperation.java
+++ library_management_system/src/main/java/com/CRUDOperation.java
@@ -1,5 +1,22 @@
 package com;
 
 public class CRUDOperation {
+    public static void addBook (string title, string author, int categoryId, int quantity){
+        String sql = "INSERT INTO BOOOKS (title, author, category_Id, quantity, available)"
+                + "VALUES (?,?,?,?,?)";
+        Connection conn = DBConnection.getConnection();
+        if (conn == null) return;
+
+        try (PreparedStatement ps == conn.prepareStatement (sql)){
+            ps.setString(parameterIndex: 1, title);
+            ps.setString(parameterIndex: 2, author);
+            ps.setInt   (parameterIndex: 3, category_Id);
+            ps.setInt   (parameterIndex: 4, quantity);
+            ps.setInt   (parameterIndex: 5, quantity);
+            int rows = ps. executeUpdate ()
+            if (rows > 0)
+        }
+
+
     
 }
